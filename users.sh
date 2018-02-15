043ec72fdd849a021f57a160000ec09157506fd6
Nathan Herrera <35143223+nathan-r2c@users.noreply.github.com>
2018-02-15 08:05:54 -0600

    Update users.sh

--- users.sh
+++ users.sh
@@ -2,14 +2,16 @@
 
 #Print out a list of users on the system
 if [ "$1" = "userlist" ]; then 
-    echo $(users)
+    users
 #Print out the UID of the specified user
 elif [ "$1" = "userinfo" ] && [ "$2" = "$2" ]; then
-    user_id="echo $(id -u $2)"
-    if [ $? -eq 0 ]; then
+    if id -u $2 &> /dev/null; [ $? = "0" ]; then
         echo $(id -u $2)
+        exit 0
     else
-        echo "Invalid User"
+        echo "Invalid User" 2> stderr.txt
+        cat stderr.txt
+        rm stderr.txt
         exit 1
     fi
 #Print out the UID of the current user
